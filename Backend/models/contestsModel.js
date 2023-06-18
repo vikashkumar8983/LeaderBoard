c5b7ffb8e874c7d16d32c040c24c467add94d3a0
vikashkumar8983 <vikashkumar8983@gmail.com>
2023-06-18 07:23:00 +0530

    removing master branch

--- Backend/models/contestsModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const contestsSchema = mongoose.Schema({
-  contestNumber: { type: String, required: true },
-  details:{type:Array}
-//   type: { type: String, required: true },
-}, {timestamps : true});
-const contestsModel = mongoose.model("contests", contestsSchema);
-module.exports = contestsModel;
